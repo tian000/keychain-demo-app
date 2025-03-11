e6fa7c4cc51a4abf3c7cdf5a0f148f1444d9bee9
tian543 <14320466+tian000@users.noreply.github.com>
2025-03-11 09:16:51 -0700

    Initial Commit

---
+++ napasapp2/napasapp2App.swift
@@ -0,0 +1,17 @@
+//
+//  napasapp2App.swift
+//  napasapp2
+//
+//  Created by nudom on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct napasapp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ napasapp2Tests/napasapp2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  napasapp2Tests.swift
+//  napasapp2Tests
+//
+//  Created by nudom on 3/11/25.
+//
+
+import Testing
+
+struct napasapp2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
